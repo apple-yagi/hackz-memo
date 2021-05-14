f73835ef7e5728f1554b1024b4717ed4293301b1
apple-yagi <applesryu@gmail.com>
2021-05-14 20:58:13 +0900

    firebase init

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,31 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    // https://www.npmjs.com/package/@nuxtjs/firebase
+    '@nuxtjs/firebase',
+  ],
+
+  // Firebase setting
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    },
+    services: {
+      auth: {
+        initialize: {
+          onAuthStateChangedAction: "auth/onAuthStateChanged"
+        },
+        ssr: false
+      }
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
